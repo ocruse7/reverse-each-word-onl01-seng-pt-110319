ba73ebfe8c43a29707713e0179a6eadaee079a87
Owen Cruse <ocruse7@gmail.com>
2019-11-15 05:05:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,4 +8,26 @@ def reverse_each_word ( phrase )
   end
   
   splitPhrase.join(" ")
+  def reverse_each_word (phrase)
+  
+  newPhrase = phrase.split
+  splitWords = []
+
+  newPhrase.collect do |words|
+    splitWords << words.reverse
+
+  end
+  splitWords.join(" ")
+   
+
+  # newPhrase = phrase.split
+  # reverseWords = []
+  # index = 0
+  # while index < newPhrase.size do
+  #   reverseWords << newPhrase[index].reverse
+  #   index += 1
+  # end
+  # newPhrase = reverseWords.join(" ")
+  # return newPhrase
+  
 end
